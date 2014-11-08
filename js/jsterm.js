3e53099d74afa112c74c4fe807a1c33280ffb798
arcticlion <david.malan@aol.com>
2014-11-08 12:12:11 +0800

    Update jsterm.js

--- js/jsterm.js
+++ js/jsterm.js
@@ -487,7 +487,7 @@
    var term = Object.create(Terminal);
    term.init(CONFIG, '/json/sample.json', COMMANDS, function() {
       term.enqueue('login')
-          .enqueue('clark')
+          .enqueue('arcticlion')
           .enqueue('******')
           .enqueue('cat file1')
           .enqueue('help')
