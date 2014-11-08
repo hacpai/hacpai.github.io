58246d50c7570d3686c9ec692f9005dc8b539af7
arcticlion <david.malan@aol.com>
2014-11-08 12:16:38 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ var CONFIG = CONFIG || {};
 CONFIG.prompt = function(cwd, user) {
    if (user)
       return '<span class="user">' + user +
-          '</span>@<span class="host">clarkduvall.com</span>:<span class="cwd">' +
+          '</span>@<span class="host">arcticlion.github.io</span>:<span class="cwd">' +
           cwd + '</span>$ ';
    return 'jsterm1.0 $ ';
 };
