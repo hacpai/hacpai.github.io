9afce8db55d88ea15135c42742ba550cf19325f7
JohnHwee <mountainsea@outlook.com>
2015-08-28 13:51:04 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ var CONFIG = CONFIG || {};
 CONFIG.prompt = function(cwd, user) {
    if (user)
       return '<span class="user">' + user +
-          '</span>@<span class="host">arcticlion.github.io</span>:<span class="cwd">' +
+          '</span>@<span class="host">hapic.github.io</span>:<span class="cwd">' +
           cwd + '</span>$ ';
    return 'Welcome $ ';
 };
